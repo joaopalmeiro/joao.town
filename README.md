9e0ea49ab0b48ad50feba5934cff8b1eea7ae42a
João Palmeiro <joaommpalmeiro@gmail.com>
2026-03-08 18:06:39 +0000

    Closes #32 , Closes #56

--- README.md
+++ README.md
@@ -19,6 +19,10 @@ npm run dev
 ```
 
 ```bash
+npm run format
+```
+
+```bash
 docker run -p 3000:3000 --rm --init -it --workdir /home/pwuser --user pwuser mcr.microsoft.com/playwright:v1.57.0-noble /bin/sh -c "npx -y playwright@1.57.0 run-server --port 3000 --host 0.0.0.0"
 ```
 
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('homepage', async ({ page }) => {
-  await page.goto('/');
+test("homepage", async ({ page }) => {
+  await page.goto("/");
 
   await expect(page).toHaveScreenshot();
 });
