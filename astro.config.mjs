3dda581db5f97aea9b4694ca48e91b36a88aea3a
João Palmeiro <joaommpalmeiro@gmail.com>
2026-03-31 15:53:46 +0100

    Closes #48

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 // @ts-check
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
@@ -12,4 +13,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://joao.town",
+  integrations: [sitemap()],
 });
