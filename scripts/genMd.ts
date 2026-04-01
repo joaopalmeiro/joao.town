ca39a0b27246c66fe35b3b45d2df3590486513ba
João Palmeiro <joaommpalmeiro@gmail.com>
2026-04-01 21:37:49 +0100

    Closes #90

--- scripts/genMd.ts
+++ scripts/genMd.ts
@@ -1,13 +1,15 @@
 import { readFile, writeFile } from "node:fs/promises";
 import { join } from "node:path";
+import { md } from "mdbox";
+import { format } from "prettier";
 
 import { Data } from "../src/models.ts";
 
-async function updateContribs(count: number): Promise<void> {
+async function updateContribs(totalContribs: number): Promise<void> {
   const file = join(import.meta.dirname, "..", "public", "index.md");
   const content = await readFile(file, "utf-8");
 
-  const updated = content.replace(/\[\d+ open source contributions\]/, `[${count} open source contributions]`);
+  const updated = content.replace(/\[\d+ open source contributions\]/, `[${totalContribs} open source contributions]`);
 
   await writeFile(file, updated, "utf-8");
 }
@@ -22,8 +24,29 @@ async function main(): Promise<void> {
   const rawData = await response.json();
   const data = Data.parse(rawData);
 
-  // TODO
-  await updateContribs(data.length);
+  const totalContribs = data.length;
+  const totalProjects = new Set(data.map((datum) => datum.repo)).size;
+
+  // https://github.com/unjs/mdbox/blob/v0.1.1/src/render.ts#L25
+  const title = md.heading("Open source contributions", 1).trim();
+
+  const metrics = md.list([`Total: ${totalContribs}`, `Projects: ${totalProjects}`]);
+
+  const table = md.table({
+    columns: ["Repo", "MR/PR", "Year"],
+    rows: data.map((datum) => {
+      return [
+        md.link(datum.url, datum.repo),
+        md.link(datum.mr_pr, datum.title),
+        datum.merged_at.getUTCFullYear().toString(),
+      ];
+    }),
+  });
+
+  const file = join(import.meta.dirname, "..", "public", "contribs.md");
+  const content = await format([title, metrics, table].join("\n\n"), { parser: "markdown" });
+
+  await Promise.all([updateContribs(totalContribs), writeFile(file, content, "utf-8")]);
 }
 
 main();
